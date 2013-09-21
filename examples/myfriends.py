fb9714ba089a63404e6726d2e6790b9ec17f12df
Jayesh <jayesh.mail@gmail.com>
2013-09-21 11:28:59 +0530

    example app changes

--- examples/myfriends.py
+++ examples/myfriends.py
@@ -1,7 +1,37 @@
 import flask
+import flask.ext.sqlalchemy
+import flask.ext.friends
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = flask.ext.sqlalchemy.SQLAlchemy(app)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode)
+    email = db.Column(db.Unicode, unique=True)
+
+
+@app.route('/login')
+def login_view():
+    pass
+
+
+@app.route('/')
+def index_page():
+    pass
+
+
+def get_friends():
+    pass
+
+
+def add_friend():
+    pass
+
+
+db.create_all()
 app.run()
 
