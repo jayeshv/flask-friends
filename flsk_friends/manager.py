243263cbf038fdb513fe6b992c13283a4761ec43
Jayesh <jayesh.mail@gmail.com>
2013-09-26 19:48:05 +0530

    functions to manager

--- flsk_friends/manager.py
+++ flsk_friends/manager.py
@@ -9,6 +9,24 @@ class FollowManager(object):
     def init_app(self, app=None, flask_sqlalchemy_db=None):
         pass
 
+    def add_group(self): 
+        pass
+
+    def drop_group(self):
+        pass
+
+    def follow(self, user1, user2, group):
+        pass
+
+    def get_followers(self, user, group):
+        pass
+
+    def get_following(self, user):
+        pass
+
+    def unfollow(self, follow):
+        pass
+
 
 class FriendshipManager(object):
     pass
