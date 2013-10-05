021cdcb75424962a22d7bec99088862d75231b7b
Jayesh <jayesh.mail@gmail.com>
2013-10-05 08:34:51 +0530

    added test functions

---
+++ tests/test_manager.py
@@ -0,0 +1,23 @@
+class TestFollowManager(object):
+    
+    def test_init_app(self):
+        pass
+
+    def test_add_group(self):
+        pass
+
+    def test_drop_group(self):
+        pass
+
+    def test_follow(self):
+        pass
+
+    def test_get_followers(self):
+        pass
+
+    def test_get_following(self):
+        pass
+
+    def test_unfollow(self):
+        pass
+
