06b1fd80c1155c233037d4d59d8cda41a5e61cf2
Jayesh <jayesh.mail@gmail.com>
2013-10-15 11:17:33 +0530

    added signals

--- flsk_friends/signals.py
+++ flsk_friends/signals.py
@@ -1,1 +1,12 @@
-pass
+import blinker
+
+signals = blinker.Namespace()
+
+follower_added = signals.signal("follower-added")
+
+request_received = signals.signal("request-received")
+
+request_accepted = signals.signal("request-accepted")
+
+request_rejected = signals.signal("request-rejected")
+
