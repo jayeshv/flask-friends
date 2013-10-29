ac20dfdacf65dd0bf8ce05a9096d8e357e57c90f
Jayesh <jayesh.mail@gmail.com>
2013-10-29 12:43:23 +0530

    info added to functions

--- flsk_friends/manager.py
+++ flsk_friends/manager.py
@@ -50,29 +50,45 @@ class FriendshipManager(object):
     def init_app(self, app=None, flask_sqlalchemy_db=None):
         pass
 
-    def get_friends(self, user):
+    def get_friends(self, user. group):
+        """Get all relations for user, filter by group if any"""
         pass
 
     def get_pending_requests(self, user):
+        """Get all requests for user with pending staus"""
+        pass
+
+    def get_pending_requests_sent(self, user):
+        """Get all requests from user with pending, rejected staus
+        ignored staus is considerd as pending"""
         pass
 
     def drop_friendship(self, friendship):
+        """Drop friendship object"""
         pass
 
     def accept_request(self, request):
+        """Accept request. Change status to accepted"""
         pass
 
     def decline_request(self, request):
+        """Reject request. Change status to declined"""
         pass
 
     def ignore_request(self, request):
+        """Ignore request. Change status to ignored"""
         pass
 
     def withdraw_request(self, request):
+        """Withdraw a pending request.
+        Can be done only iff peding state"""
         pass
 
     def get_route(self, from_user, to_user):
+        """Get shortest degree of relation between two users.
+        Returns all routes if more than one exists with same degree"""
         pass
 
     def get_common_friends(self, user1, user2):
+        """Returns friends commmon to user1 and user2"""
         pass
